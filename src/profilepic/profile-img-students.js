8bbdec4c93a80b0f9c75b4c36ae9d3fea69dcd58
koush <koushikbabu003@gmail.com>
2026-01-24 11:22:25 +0530

    Koushikbabu: Pushed to Prod 🚀

--- src/profilepic/profile-img-students.js
+++ src/profilepic/profile-img-students.js
@@ -5,6 +5,7 @@ import crypto from "crypto";
 import verify from "../common/middleware.js";
 import { Student } from "../accounts/creation-students.js";
 import { s3Client, MINIO_BUCKET } from "../common/minio-cfg.js";
+import { URL } from "url";
 
 import { S3RequestPresigner } from "@aws-sdk/s3-request-presigner";
 import { HttpRequest } from "@aws-sdk/protocol-http";
@@ -13,22 +14,26 @@ import { Hash } from "@aws-sdk/hash-node";
 
 const router = express.Router();
 
+
 async function signedUrl(objectKey) {
+  const endpoint = new URL(process.env.MINIO_ENDPOINT);
+
   const signer = new S3RequestPresigner({
     ...s3Client.config,
     sha256: Hash.bind(null, "sha256"),
   });
 
   const req = new HttpRequest({
-    ...s3Client.config,
-    protocol: "http:",
-    hostname: process.env.MINIO_HOST,
+    protocol: endpoint.protocol,     // http:
+    hostname: endpoint.hostname,      // 20.6.95.37
+    port: Number(endpoint.port),      // 9000 ✅
     method: "GET",
     path: `/${MINIO_BUCKET}/${objectKey}`,
   });
 
   return formatUrl(await signer.presign(req, { expiresIn: 300 }));
 }
+ 
 
 // ----------------------------------------------
 // STUDENT VIEW OWN PHOTO (proxy)
